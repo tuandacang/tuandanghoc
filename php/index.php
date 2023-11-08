ea566970ae5bc9c79d3a7a56c6901addaa645be1
tuandacang <tvu05035@gmail.com>
2023-11-08 15:03:15 +0700

    tuan

--- php/index.php
+++ php/index.php
@@ -1,3 +1,11 @@
-<?php 
-echo "Tuan anh";
+<?php
+$Subject = "Lập trình ứng dụng web";
+$unversityName = "HUMG";
+$firstNumber = 2;
+$secondtNumber = 2;
+
+echo "Học ". $Subject . " ở phòng 502 tại trường " . $unversityName . "<br>";
+echo "Học $Subject ở phòng 502 tại trường $unversityName <br>";
+echo "<h1> $unversityName </h1>";
+echo $firstNumber + $secondtNumber;
 ?>
