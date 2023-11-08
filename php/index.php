8018b3cdc29ec647dd3d626683f221ffbe61c1fe
tuandacang <tvu05035@gmail.com>
2023-11-08 15:19:46 +0700

    vidu

--- php/index.php
+++ php/index.php
@@ -1,11 +1,8 @@
 <?php
-$Subject = "Lập trình ứng dụng web";
-$unversityName = "HUMG";
-$firstNumber = 2;
-$secondtNumber = 2;
-
-echo "Học ". $Subject . " ở phòng 502 tại trường " . $unversityName . "<br>";
-echo "Học $Subject ở phòng 502 tại trường $unversityName <br>";
-echo "<h1> $unversityName </h1>";
-echo $firstNumber + $secondtNumber;
-?>
+$longAccount = 10.000.000;
+echo " So tien trong tai khoan Long: $longAccount";
+$HUMGPay = 10.000.000;
+echo "HUMG chuyen tien: $HUMGPay";
+$longAccount = $longAccount + $HUMGPay;
+echo "So tien trong tai khoan Long: $longAccount";
+?>  
