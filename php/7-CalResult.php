368cd916e038a8a7b82b9616b2a1922598335093
tuandacang <tvu05035@gmail.com>
2023-11-10 17:55:20 +0700

    tim nghiem phuong trinh

--- php/7-CalResult.php
+++ php/7-CalResult.php
@@ -1,15 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title></title>
+<title> Ket qua giai phuong trinh!!</title>
 </head>
 <body>
-   <?php
-   $text = $_GET['vanban'];
-   $kytu = $_GET['kytu'];
+<?php
+   $B=$_GET["b"];
+   $C=$_GET["c"];
+   $A=$_GET["a"];
+   if($A==0)
+      if($B==0)
+         if($C==0)
+            echo "Phương trình có vô số nghiệm.";
+         else
+            echo "Phương trình này tôi xin bó tay.";
+      else
+      {
+         echo "Phương trình bậc 1 có dạng: ".$B." X + ".$C." = 0 <br/>";
+         echo "Phương trình có nghiệm: "."X = ". -$C/$B. "<br/>";
+      }
+   else
+   {
+      echo "Phương trình có dạng: ".$A." X2 + ".$B." X + ".$C." = 0 <br/>";
+      $delta=pow($B,2)-4*$A*$C; 
+      if($delta==0)
+      {
+         echo "Phương trình có nghiệm kép X1 = X2 = ".-$B/(2*$A)."<br/>";
+      }
+      else
+      {
+         if($delta<0)
+         {
+            echo "Phương trình vô nghiệm.";
+         }
+         else
+         {
+            $X1=(-$B+sqrt($delta))/(2*$A);
+            $X2=(-$B-sqrt($delta))/(2*$A);
+            echo "Phương trình có 2 nghiệm: <br/>";
+            echo "X1 = ".$X1."<br/>";
+            echo "X2 = ".$X2."<br/>";
+         }
+      }
+   }
+?>
 
-    echo substr_count($text, $kytu);
-    ?>
 </body>
 </html>
--- php/index.php
+++ php/index.php
@@ -6,10 +6,12 @@
 </head>
 <body>
     <form action="7-Calresult.php" method="get">
-        <p>Văn bản</p>
-        <input type="text" name="vanban" ><br>
-        <p>Ký tự cần kiểm tra</p>
-        <input type="text" name="kytu"><br>
+        <p>a</p>
+        <input type="number" name="a" ><br>
+        <p>b</p>
+        <input type="number" name="b"><br>
+        <p>c</p>
+        <input type="number" name="c"><br>
         <p></p>
         <input type="submit" value="Add">
     </form>
