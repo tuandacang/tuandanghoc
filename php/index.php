46c21c7dfd01892532d95ec2a69150d87d604f12
tuandacang <tvu05035@gmail.com>
2023-11-22 07:12:24 +0700

    slide 39

--- php/index.php
+++ php/index.php
@@ -2,27 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
 </head>
-<body>
-<form action="7-Calresult.php" method="get">
-    <div class="tad-v-1">
-        <div class="tad-container">
-            <h2>Đăng ký thành viên</h2>
-        </div>
-            <label for=""><b>First Name</b></label>
-            <input type="text" name="Fname" ><br>
-            <p>
-            <label for=""><b>Last Name</b></label>
-            <input type="text" name="Lname"><br>
-            <p>
-            <label for=""><b>Email</b></label>
-            <input type="text" name="e-mail"><br>
-            <p>
-            <label for=""><b>Phone</b></label>
-            <input type="number" name="phone"><br>
-            <p>
-            <input type="submit" value="SUBMIT">
-        </form>
-</body>
+   <body> 
+        <form action="5-formGet.php" method="get">
+           <p>Name: </p>
+           <input type="text" name="name"><br>
+           <p>E-mail: </p>
+           <input type="text" name="email"><br>
+           <p></p>
+           <input type="submit">
+   </body>
 </html>
