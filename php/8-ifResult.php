d21cba0eeceb03571fe00a37a2a850431e39773e
tuandacang <tvu05035@gmail.com>
2023-12-05 19:21:52 +0700

    slide 40

--- php/8-ifResult.php
+++ php/8-ifResult.php
@@ -1,32 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-</head>
-   <body> 
-      <?php
-         $USD = 22300;
-         $EUR = 27300;
-         $AUD = 17000;
-         $JPY = 120;
-         $amount = $_GET['response'];
-         echo " $amount USD is equal ";
-         if ($_GET["response"] == "USD")
-         {
-            echo $amount * $USD;
-         }
-         elseif ($_GET["response"] == "EUR") { 
-            echo $amount * $EUR;
-         }
-         elseif ($_GET["response"] == "AUD") {
-            echo $amount * $AUD;
-         }
-         elseif ($_GET["response"] == "JPY") {
-            echo $amount * $JPY;
-         }
+<?php
+   $severname = "localhost";
+   $username = "root";
+   $password = "";
+   $db = "myDB";
 
-         echo "VND";
-      ?>
-   </body>
-</html>
+   $conn = new mysqli($severname, $username, $password, $db);
+
+   if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connection_error);
+   }
+   echo "Connected successfully <br>";
+?>
