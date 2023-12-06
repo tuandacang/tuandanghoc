d0cc28589a1cda1d46e65e4c0dc227bffde8b716
tuandacang <tvu05035@gmail.com>
2023-12-06 14:48:16 +0700

    slide 47

---
+++ php/1-insert.php
@@ -0,0 +1,9 @@
+<?php
+   require 'connect.php';
+
+   $sql = "INSERT INTO flights (Origin,Destination,Duration) VALUES ('Hà Nội', 'Paris', '4000')";
+
+   $conn->query($sql);
+
+   $conn->close();
+?>
---
+++ php/connect.php
@@ -0,0 +1,13 @@
+<?php
+    $severname = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "myDB";
+
+    $conn = new mysqli($severname, $username, $password, $db);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected succseefully <br>";
+?>
