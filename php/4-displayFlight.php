9606f721cbb22c215c00a65a55c3ddd443f67d38
tuandacang <tvu05035@gmail.com>
2023-12-08 09:59:01 +0700

    slide 69

--- php/4-displayFlight.php
+++ php/4-displayFlight.php
@@ -1,30 +1,45 @@
-<?php
-if ($result->num_rows > 0)
-{
-    echo"<table border='1' width=50% align='center'>
-        <caption> <b> Bảng thông tin chuyến bay</caption>
-        <tr>
-            <th>ID</th>
-            <th>Origin</th>
-            <th>Destination</th>
-            <th>Duration</th>
-        </tr>";
-    while($row = $result->fetch_assoc())
-    {
-        if ($row["id"]%2==0)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    
+    <?php
+        if($result -> num_rows > 0)
         {
-            echo "<tr class='odd'><td>" . $row["id"]. "</td>";
+            echo"
+            <table border = '1' width =50% align ='center'>
+            <caption> <b> Bảng thông tin chuyến bay </b></caption>
+            <tr>
+                <th>ID</th>
+                <th>Origin</th>
+                <th>Destination</th>
+                <th>Duration</th>
+            </tr>";
+            while($row = $result -> fetch_assoc())
+            {
+                if($row['id'] % 2 ==0)
+                {
+                    echo"<tr class ='odd'><td>". $row['id']. "</td>";
+                }
+                else
+                {   
+                    echo"<tr class ='even'><td>". $row['id']. "</td>";
+                }
+                echo"<td>" . $row['origin']. "</td>".
+                    "<td>" . $row['destination']. "</td>".
+                    "<td>" . $row['duration']. "</td></tr>";
+            }
         }
-        else
-        {
-            echo "<tr class='even'><td>" . $row["id"]. "</td>";
+        else{
+            echo"No flight in database";
         }
-        echo "<td>" . $row["origin"]. "</td>" . 
-            "<td>" . $row["destination"]. "</td>" .
-            "<td>" . $row["duration"]. "</td>";
-    }
-}
-else {
-    echo "No flight in database";
-}
-?>
+    ?>
+    <h1>Vũ Tuấn Anh 21210503596</h1>
+    
+</body>
+</html>
--- php/searchflight.php
+++ php/searchflight.php
@@ -2,33 +2,44 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Search Flight</title>
-    <link rel="stylesheet" type="text/css" href="4-search.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="4-search.css">
+    <title>Document</title>
 </head>
-    <body>
-    <h1> Search Flight</h1>
-    <form method="get" action="" name="register">
-            Origin <input type="text" name="origin" /><br>
-            Destination <input type="text" name="destination" /><br>
-            <input type="submit" value="Search" /><br>
-        </form>
-        <?php
-            if (isset($_GET['origin']))
+<body>
+    <h2>Search Flight</h2>
+    <form action="" method = 'get' name ='register'>
+        Origin <input type="text" name='origin'><br>
+        Destination <input type="text" name ='destination'><br>
+        <input type="submit" value='Search'><br>
+
+    </form>
+    <?php
+            if(isset($_GET['origin']))
             {
+                require 'connect.php';
                 $origin = $_GET['origin'];
                 $destination = $_GET['destination'];
-
-                require 'connect.php';
-                mysqli_set_charset($conn, 'UTF8');
-
-                $sql = "SELECT * FROM flight WHERE origin = '$origin' and destination = '$destination'";
-
+                // echo"$destination";
+                // echo"$origin";
+                
+                mysqli_set_charset($conn, 'UTF8'); //sua loi tieng viet
+    
+                $sql = "SELECT * FROM flights WHERE origin = '$origin' and destination = '$destination'";
+                // $sql = 'SELECT * FROM flights ';
                 $result = $conn->query($sql);
-
-                require('4-displayFlight.php');
-
+    
+                require ('4-displayFlight.php');
                 $conn->close();
+                
+                
             }
-        ?>
-    </body>
+           
+            
+            
+
+        
+    ?>
+    
+</body>
 </html>
